4111796a9f49bdc7b2cdfb4888e5c40e49e936df
Harry Hubbell <harry.hubbell@gmail.com>
2026-07-27 21:58:41 -0400

    Add support for more in the event less is not available

--- src/pager.zig
+++ src/pager.zig
@@ -10,17 +10,16 @@ const SIG = posix.SIG;
 
 /// Known (non-exhaustive) list of pagers that we will pipe out to for long
 /// results. The order matters here, as the search for an appropriate pager
-/// will return as soon as a match is found in PATH
+/// will return as soon as a match is found in PATH. Therefore, this enum
+/// is in preferential order: less > more > nopager.
 pub const PagerType = enum {
-    less, nopager
+    less, more, nopager
 };
 
 /// Determine if a pager is available in PATH to page long output. Return
 /// a value from an enum of known pagers. This is not exhaustive. This may
 /// be the wrong design to support arbitrary pagers provided by the user,
-/// but we'll get there
-///
-/// Also right now only less is supported!
+/// but we'll get there.
 pub fn whichPager(io: Io, env: *Environ.Map) PagerType {
     const path = env.get("PATH") orelse return .nopager;
 
@@ -41,9 +40,9 @@ pub fn whichPager(io: Io, env: *Environ.Map) PagerType {
             const st = std.Io.File.stat(handle, io) catch continue;
 
             if (st.kind == .file) {
-                // When paging results, we need to disable SIGPIPE if we quit the pager
-                // without consuming the entire result set. This is a similar approach
-                // used in the Postgres cli
+                // When paging results, we need to disable SIGPIPE if we quit
+                // the pager without consuming the entire result set. This is a
+                // similar approach used in the Postgres cli
                 disableSigPipe();
 
                 return typ;
@@ -58,6 +57,7 @@ pub fn whichPager(io: Io, env: *Environ.Map) PagerType {
 pub fn page(io: Io, pager: PagerType, data: []const u8) !void {
     switch(pager) {
         .less => try lessPipe(io, data),
+        .more => try morePipe(io, data),
         .nopager => try writeStdout(io, data)
     }
 }
@@ -71,6 +71,24 @@ fn lessPipe(io: Io, data: []const u8) !void {
         .stdout = .inherit,
         .stderr = .inherit});
 
+    try writeChild(io, &child, data);
+}
+
+/// Provide a pager child process for outputs using `more`. Requires dependency
+/// `more`.
+fn morePipe(io: Io, data: []const u8) !void {
+    var child = try std.process.spawn(io, .{
+        .argv = &[1][]const u8 {"more"},
+        .stdin = .pipe,
+        .stdout = .inherit,
+        .stderr = .inherit});
+
+    try writeChild(io, &child, data);
+}
+
+/// Wite output unbuffered to a child process pipe. In the event the pipe
+/// closes, clean up and return, making the WriteFailed error
+fn writeChild(io: Io, child: *std.process.Child, data: []const u8) !void {
     var writer = child.stdin.?.writer(io, &.{});
 
     // WARNING: 
@@ -94,10 +112,10 @@ fn lessPipe(io: Io, data: []const u8) !void {
     child.stdin.?.close(io);
 
     // NOTE:
-    //  Using -F results in a thread panic if the input data is too small
-    //  without explicitly doing this action below. I believe this is caused
-    //  by the wait function trying to clean up a non-existant fd. Setting
-    //  it to null solves this problem.
+    //  Using `less -F` results in a thread panic if the input data is too
+    //  small without explicitly doing this action below. I believe this is
+    //  caused by the wait function trying to clean up a nonexistent fd.
+    //  Setting it to null solves this problem.
     child.stdin = null;
 
     _ = try child.wait(io);
